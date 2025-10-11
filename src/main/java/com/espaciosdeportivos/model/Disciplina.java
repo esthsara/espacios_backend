37a070fb75b015d3ad733e793937e334fe42d4c6
Mysha_kiss <lopezhuancajosuemisael@gmail.com>
2025-10-11 18:24:50 -0400

    Resuelve conflicto: combina anotaciones lombok y mas, etc

--- src/main/java/com/espaciosdeportivos/model/Disciplina.java
+++ src/main/java/com/espaciosdeportivos/model/Disciplina.java
@@ -6,7 +6,7 @@ import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 import java.time.LocalDateTime;
 import java.util.List;
-//J
+//OK//
 @Entity
 @Table(name = "disciplina")
 @Getter
